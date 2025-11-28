e70e900245cd4ffd188ba80d3734363e4e6d7788
ups-q3 <ups2005@yandex.ru>
2025-11-28 23:26:19 +0200

    Update 7segment_gpio.h

--- components/7segment_gpio/7segment_gpio.h
+++ components/7segment_gpio/7segment_gpio.h
@@ -1,13 +1,5 @@
 /**
- * @file lcd_digits.h
- * @author Anton Sergunov
- * @brief
- * @version 0.1
- * @date 2023-11-10
- *
- * @copyright Anton Sergunov (c) 2023
- *
- * Based on ac_dimmer for timers and max7219 display code
+ * @file 7segment_gpio.h
  */
 
 #pragma once
@@ -15,10 +7,6 @@
 #include "esphome.h"
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "esphome/core/helpers.h"
-#include "esphome/core/time.h"
-
-#include <functional>
 #include <vector>
 
 #ifdef USE_ESP32_FRAMEWORK_ARDUINO
@@ -27,9 +15,7 @@
 
 namespace esphome {
 namespace lcd_digits {
-class LcdDigitsComponent;
 
-using lcd_digits_writer_t = std::function<void(LcdDigitsComponent &)>;
 constexpr uint8_t max_digit_count = 4;
 
 struct LcdData {
@@ -41,73 +27,56 @@ struct LcdData {
 enum DisplayType { CommonAnode, CommonCathode };
 
 struct LcdDigitsData : LcdData {
-  std::vector<GPIOPin *> digit_pins = {
-      nullptr}; // by default it will expect one pin connected to power rail so
-                // don't need any actions from library side.
+  std::vector<GPIOPin *> digit_pins;
   std::vector<GPIOPin *> segment_pins;
   GPIOPin *colon_pin = nullptr;
   GPIOPin *degree_pin = nullptr;
+
   uint8_t cycles_to_skip = 0;
   uint8_t current_frame = 0;
-  uint16_t blank_delay_us = 120;  // час гасіння, наприклад 120 мкс
-  /**
-   * @brief Increase on time proptional to lighting items
-   * Usable if you have resistros on digit pins and swithcing the digits
-   */
+
   bool compensate_brightness = false;
-
-  DisplayType display_type = CommonAnode;
-
-  /**
-   * @brief Iterate digits or segments
-   *
-   * If you have resistors on digits you can iterate segemnts to keep brightness
-   * the same
-   */
   bool iterate_digits = true;
 
   uint8_t intensity_delay = 0;
 
-  void IRAM_ATTR HOT timer_interrupt();
+  // ➜ НОВЕ: затримка перед вмиканням цифри
+  uint16_t blank_delay_us = 50;
+
+  void IRAM_ATTR timer_interrupt();
 };
 
-//   a
-// f   b
-//   g
-// e   c
-//   d  .
 class LcdDigitsComponent : public PollingComponent {
 public:
   enum Mode { BufferMode, ProgressMode, DisabledMode };
 
-  void set_degree_pin(GPIOPin *arg);
-  void set_colon_pin(GPIOPin *arg);
-  void set_segment_pins(std::vector<GPIOPin *> segment_pins);
-  void set_digit_pins(std::vector<GPIOPin *> digit_pins);
-  void set_writer(lcd_digits_writer_t &&writer);
+  void set_segment_pins(std::vector<GPIOPin *> pins);
+  void set_digit_pins(std::vector<GPIOPin *> pins);
+  void set_colon_pin(GPIOPin *pin);
+  void set_degree_pin(GPIOPin *pin);
+
   void set_display_type(DisplayType arg);
   void set_compensate_brightness(bool arg);
   void set_iterate_digits(bool arg);
   void set_intensity(uint8_t arg);
 
+  // ➜ НОВЕ: setter затримки
+  void set_blank_delay_us(uint16_t v) {
+    interrupt_data_.blank_delay_us = v;
+  }
+
   void setup() override;
   void update() override;
   void dump_config() override;
 
-  uint8_t print(uint8_t start_pos, const char *str);
   uint8_t print(const char *str);
-  uint8_t printf(uint8_t pos, const char *format, ...);
-  /**
-   * @brief Set the raw value
-   *
-   * \a raw are 8 bytes low byte becomes the right most one.
-   * Each byte is in format `0b.abcdefg`
-   * @param raw
-   */
+  uint8_t print(uint8_t pos, const char *str);
+  uint8_t printf(uint8_t pos, const char *fmt, ...);
+
   void set_raw(uint64_t raw);
-  void strftime(uint8_t pos, const char *format, ESPTime time);
-  void set_degree_on(bool arg = true);
-  void set_colon_on(bool arg = true);
+
+  void set_colon_on(bool on = true);
+  void set_degree_on(bool on = true);
 
   void set_mode(Mode mode);
   void set_progress(float progress);
@@ -115,9 +84,10 @@ public:
 private:
   static constexpr auto TAG = "lcd_digits";
   hw_timer_t *timer = nullptr;
-  optional<lcd_digits_writer_t> writer_{};
+
   LcdDigitsData interrupt_data_;
   LcdData display_data_;
+
   Mode mode_ = DisabledMode;
 };
 
